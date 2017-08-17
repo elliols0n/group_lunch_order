faa91e4345eab13eca971adef0aeb858be542a06
Raghu Betina <raghu@firstdraft.com>
2017-08-17 00:53:26 +0000

    Whitelist columns in Order for ActiveAdmin

--- app/admin/order.rb
+++ app/admin/order.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Order do
 
+ permit_params :user_id, :dish_id, :user_comment, :group_order_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
