b9cd329ea7fd5cc877481e3a0e6dd6ea38b0a6a1
Raghu Betina <raghu@firstdraft.com>
2017-08-17 00:53:47 +0000

    Only order user can modify order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,14 @@
 class OrdersController < ApplicationController
+  before_action :current_user_must_be_order_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_order_user
+    order = Order.find(params[:id])
+
+    unless current_user == order.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @orders = Order.all
 
